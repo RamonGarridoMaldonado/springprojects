97ad41886a5752373a7cddf0aeac7ff9994eb943
Ramon Garrido <ramongarrido430@gmail.com>
2022-11-03 10:31:43 +0100

    ver producto

--- tienda/src/main/java/com/rgarmal/springproject/tienda/model/Producto.java
+++ tienda/src/main/java/com/rgarmal/springproject/tienda/model/Producto.java
@@ -63,5 +63,27 @@ public class Producto {
         this.fecha = fecha;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + codigo;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Producto other = (Producto) obj;
+        if (codigo != other.codigo)
+            return false;
+        return true;
+    }
+
     
 }
