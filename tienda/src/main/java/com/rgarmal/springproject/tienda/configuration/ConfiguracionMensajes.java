d0015282ad1867264412c89bdfdda62f8c8a0788
Ramon Garrido <ramongarrido430@gmail.com>
2022-11-11 14:39:25 +0100

    11-11-2022 #2

--- tienda/src/main/java/com/rgarmal/springproject/tienda/configuration/ConfiguracionMensajes.java
+++ tienda/src/main/java/com/rgarmal/springproject/tienda/configuration/ConfiguracionMensajes.java
@@ -8,10 +8,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
-public class ConfiguracionMensajes {
+public class ConfiguracionMensajes implements WebMvcConfigurer {
 
     @Bean
     public LocaleResolver localeResolver() {
@@ -26,5 +29,18 @@ public class ConfiguracionMensajes {
         messageSource.setBasename("classpath:i18n/messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang_config");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        WebMvcConfigurer.super.addInterceptors(registry);
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
